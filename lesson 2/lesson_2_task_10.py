f8bcdbf4e5176726bb58a74d230d14c88fb4f8f5
kim563 <154543324+kim563@users.noreply.github.com>
2024-05-23 00:50:56 +0700

    Revert "Lesson2"

--- lesson 2/lesson_2_task_10.py
+++
@@ -1,7 +0,0 @@
-def bank(x, y):
-    summa_vklada = x
-    for n in range(1, y):
-        summa_vklada = summa_vklada + summa_vklada * 0.1
-    return summa_vklada
-
-print(bank(100000, 12))
--- lesson 2/lesson_2_task_7.py
+++
@@ -1,7 +0,0 @@
-lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-summa = 0
-
-for x in lst:
-    summa = summa + x
-print(summa)
-    
