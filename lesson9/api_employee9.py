9644cbad9ffee948845c27c1f6577439a9e2f8df
kim563 <anastasiasam7053@gmail.com>
2024-08-16 00:37:48 +0700

    lesson9-fix

--- lesson9/api_employee9.py
+++ lesson9/api_employee9.py
@@ -1,14 +1,30 @@
 import requests
+import allure
 
 
 class Employee:
+    """ Класс для работы с API сотрудников """
 
-    # Инициализация
-    def __init__(self, url) -> None:
+    def __init__(self, url: str) -> None:
+        """
+        Инициализация обьекта.
+        
+        args:
+        url - базовый URL для API.
+        """
         self.url = url
 
-    # Получить токен авторизации
-    def get_token(self, user='donatello', password='does-machines'):
+    @allure.step("Получение токена авторизации для пользователя {user}")
+    def get_token(self, user: str='donatello', password: str='does-machines'):
+        """
+        Авторизация на сайте.
+
+        args:
+        user - имя пользователя,
+        password - пароль пользователя.
+
+        return: Токен авторизации.
+        """
         creds = {
             "username": user,
             "password": password
@@ -16,26 +32,59 @@ class Employee:
         resp = requests.post(self.url + '/auth/login', json=creds)
         return resp.json()["userToken"]
 
-    # Получить список сотрудников для компании
-    def get_employee_list(self, id_company):
+    @allure.step("Получение списка сотрудников для компании с {id_company}")
+    def get_employee_list(self, id_company: int):
+        """
+        Получить список сотрудников для компании.
+
+        args:
+        id_company - ID компании.
+
+        return: Список сотрудников.
+        """
         resp = requests.get(self.url + '/employee', params=id_company)
         return resp
 
-    # Добавить нового сотрудника:
-    def create_employee(self, description):
+    @allure.step("Добавление нового сотрудника с описанием {description}")
+    def create_employee(self, description: dict):
+        """
+        Добавить нового сотрудника.
+
+        args:
+        description - описание сотрудника в формате JSON.
+
+        return: Ответ после создания сотрудника.
+        """
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
         resp = requests.post(self.url + '/employee',
                              json=description, headers=my_headers)
         return resp
 
-    # Получение сотрудника по id
-    def get_employee(self, user_id):
+    @allure.step("Получение сотрудника по {user_id}")
+    def get_employee(self, user_id: int):
+        """
+        Получение информации о сотруднике по id.
+
+        args:
+        user_id: id сотрудника.
+
+        return: Ответ с информацией о сотруднике.
+        """
         resp = requests.get(self.url + '/employee/' + str(user_id))
         return resp
 
-    # Изменить информацию о сотруднике
-    def update_employee(self, user_id, description):
+    @allure.step("Изменение информации о сотруднике с {user_id}")
+    def update_employee(self, user_id: int, description: dict):
+        """
+        Изменить информацию о сотруднике.
+
+        args:
+        user_id - id сотрудника.
+        description - новое описание сотрудника в формате JSON.
+
+        return: Ответ после изменения информации о сотруднике.
+        """
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
         resp = requests.patch(self.url + '/employee/' + str(user_id),
--- lesson9/db_employee.py
+++ lesson9/db_employee.py
@@ -1,42 +1,137 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from sqlalchemy.orm import Session
+import allure
 
-
-class Data_Base:
+class DataBase:
+    """ Класс для работы с базой данных сотрудников и компаний """
 
     __scripts = {
         "select_for_id": text("select * from employee where company_id = :id"),
         "delete_by_id": text("delete from employee where id = :id_to_delete"),
+        "insert_company": text("INSERT INTO company (\"name\") values (:new_name);"),
         "insert_employee": text("INSERT into employee (first_name, \
             last_name, phone, company_id) values (:first_name, \
             :last_name, :phone, :company_id)"),
         "get_max_id": text("select MAX(\"id\") from employee"),
+        "get_max_id_company": text("select MAX(\"id\") from company"),
+        "delete_by_id_company": text("delete from company where id =:id_to_delete"),
     }
 
-    def __init__(self, connection_string):
-        self.db = create_engine(connection_string)
+    def __init__(self, connection_string: str):
+        """
+        Инициализация обьекта.
 
-    def get_employee_list(self, id_company):
-        sql = self.__scripts["select_for_id"]
-        return self.db.connect().execute(sql, {"id": id_company}).fetchall()
+        args:
+        connection_string - строка подключения к базе данных.
+        """
+        self.db = Session(create_engine(connection_string))
 
-    def delete_employee(self, id):
-        sql = self.__scripts["delete_by_id"]
-        self.db.connect().execute(sql, {"id_to_delete": id})
+    @allure.step("Создание компании с именем {name}")
+    def create_company(self, name: str):
+        """
+        Создать новую компанию.
 
-    def create_employee(self, firstname, lastname, phone, company_id):
-        sql = self.__scripts["insert_employee"]
-        self.db.connect().execute(
-            sql,
-            {
-                "first_name": firstname,
-                "last_name": lastname,
-                "phone": phone,
-                "company_id": company_id
-            }
+        args:
+        name - имя новой компании.
+        """
+        with self.db.begin():
+            sql = self.__scripts["insert_company"]
+            self.db.execute(sql, {"new_name": name})
+            self.db.commit()
+
+    @allure.step("Получение максимального ID компании")
+    def get_max_id_company(self):
+        """
+        Получить максимальный ID компании.
+
+        return: Максимальный ID компании.
+        """
+        with self.db.begin():
+            sql = self.__scripts["get_max_id_company"]
+            result = self.db.execute(sql).fetchall()[0][0]
+            self.db.commit()
+            return result
+
+    @allure.step("Получение списка сотрудников компании с {id_company}")
+    def get_employee_list(self, id_company: int):
+        """
+        Получить список сотрудников для компании.
+
+        args:
+        id_company - id компании.
+
+        return: Список сотрудников.
+        """
+        with self.db.begin():
+            sql = self.__scripts["select_for_id"]
+            result = self.db.execute(sql, {"id": id_company}).fetchall()
+            self.db.commit()
+            return result
+    
+    @allure.step("Удаление компании с {id}")
+    def delete_company(self, id: int):
+        """
+        Удалить компанию по id.
+
+        args:
+        id - id компании.
+        """
+        with self.db.begin():
+            sql = self.__scripts["delete_by_id_company"]
+            self.db.execute(sql, {"id_to_delete": id})
+            self.db.commit()
+
+    @allure.step("Удаление сотрудника с {id}")
+    def delete_employee(self, id: int):
+        """
+        Удалить сотрудника по id.
+
+        args:
+        id - id сотрудника.
+        """
+        with self.db.begin():
+            sql = self.__scripts["delete_by_id"]
+            self.db.execute(sql, {"id_to_delete": id})
+            self.db.commit()
+
+    @allure.step(
+            "Создание сотрудника с параметрами: Имя {firstname}, \
+            Фамилия {lastname}, Телефон {phone}, ID компании {company_id}"
         )
+    def create_employee(
+        self, firstname: str, lastname: str, phone: str, company_id: int
+        ):
+        """
+        Создать нового сотрудника.
 
+        args:
+        firstname - имя сотрудника.
+        lastname - фамилия сотрудника.
+        phone - телефон сотрудника.
+        company_id id компании.
+        """
+        with self.db.begin():
+            sql = self.__scripts["insert_employee"]
+            self.db.execute(
+                sql,
+                {
+                    "first_name": firstname,
+                    "last_name": lastname,
+                    "phone": phone,
+                    "company_id": company_id
+                })
+            self.db.commit()
+
+    @allure.step("Получение максимального id сотрудника")
     def get_max_id_employee(self):
-        sql = self.__scripts["get_max_id"]
-        resp = self.db.connect().execute(sql).fetchall()[0][0]
-        return resp
+        """
+        Получить максимальный id сотрудника.
+
+        return: Максимальный id сотрудника.
+        """
+        with self.db.begin():
+            sql = self.__scripts["get_max_id"]
+            resp = self.db.execute(sql).fetchall()[0][0]
+            self.db.commit()
+            return resp
